f3df72c03fdeb7946f2a972d5a2c5542d5e393ca
corocn <crsk.plmzh@gmail.com>
2018-03-22 20:52:16 +0900

    add netlify-identity-widget

---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="netlify-modal"></div>  
+</template>
+
+<script>
+import netlifyIdentity from "netlify-identity-widget";
+
+export default {
+  mounted() {
+    netlifyIdentity.init({
+      container: "#netlify-modal",
+    });
+    netlifyIdentity.open();
+  }
+};
+</script>
